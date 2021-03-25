04901f2047c199065816bfb94eb3059c15fce447
Jul <69659361+julsu2905@users.noreply.github.com>
2021-03-25 07:50:03 +0700

    update Navigator

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import "antd/dist/antd.css";
 import "./App.css";
 import { Layout, Col, Row } from "antd";
+import {BrowserRouter as Router} from 'react-router-dom';
 import Navigator from "./components/Navigator";
-
+const {Route} = Router;
 const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
-    <Layout>
-      <Header className="header-wrapper">
-        <Navigator />
-      </Header>
-      <Content className="content-wrapper"></Content>
-      <Footer>Footer</Footer>
-    </Layout>
+      <Layout>
+        <Header className="header-wrapper">
+          <Navigator />
+        </Header>
+        <Content className="content-wrapper"></Content>
+        <Footer>Footer</Footer>
+      </Layout>
+
   );
 }
 
--- client/src/components/Navigator.js
+++ client/src/components/Navigator.js
@@ -1,59 +1,71 @@
 import React, { useState } from "react";
-import { Col, Menu, Row } from "antd";
+import { Link, BrowserRouter as Router, useHistory } from "react-router-dom";
+import { Col, Menu, Row, Input } from "antd";
 import {
   HomeOutlined,
-  YoutubeOutlined,
-  ContainerOutlined,
   UserOutlined,
-  ShoppingCartOutlined
+  ShoppingCartOutlined,
 } from "@ant-design/icons";
 import "./component-css/Navigator.css";
+const { Search } = Input;
 
 const Navigator = () => {
+  const history = useHistory();
   const [current, setCurrent] = useState("home");
   const handleClick = (e) => {
-    console.log("click ", e);
+    console.log("click ", e.key);
     setCurrent(e.key);
+    history.push(`/${e.key}`);
+  };
+  const onSearch = (e, value) => {
+    console.log(value);
   };
   return (
     <Row className="nav-wrapper">
-      <Col span={14} className="left-nav">
-        <Menu
-          className="navigator"
-          onClick={handleClick}
-          selectedKeys={[current]}
-          mode="horizontal"
-        >
-          <Menu.Item key="home" icon={<HomeOutlined />}>
-            Trang Chủ
-          </Menu.Item>
-          <Menu.Item key="tintuc" icon={<ContainerOutlined />}>
-            Tin Tức
-          </Menu.Item>
-          <Menu.Item key="video" icon={<YoutubeOutlined />}>
-            Video
-          </Menu.Item>
-          <Menu.Item key="hinhanh">Hình Ảnh</Menu.Item>
-          <Menu.Item key="blog">Bếp trưởng gia đình</Menu.Item>
-        </Menu>
-      </Col>
-      <Col span={10} className="right-nav">
-        <Menu
-          className="navigator"
-          onClick={handleClick}
-          selectedKeys={[current]}
-          mode="horizontal"
-        >
-          <Menu.Item key="policy">Bảo Hành</Menu.Item>
-          <Menu.Item key="history">Kiểm tra Đơn hàng</Menu.Item>
-          <Menu.Item key="login" icon={<UserOutlined />}>
-            Đăng nhập
-          </Menu.Item>
-          <Menu.Item key="cart" icon={<ShoppingCartOutlined />}>
-            Giỏ hàng
-          </Menu.Item>
-        </Menu>
-      </Col>
+      <Router>
+        <Col span={6} className="left-nav">
+          <Menu
+            className="navigator"
+            onClick={handleClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+          >
+            <Menu.Item key="home" icon={<HomeOutlined />}>
+              Trang Chủ
+            </Menu.Item>
+          </Menu>
+        </Col>
+        <Col className="logo" offset={2} span={3}>
+          <Link to="/home">
+            <img
+              alt="logo-home"
+              width="90"
+              height="80"
+              src="../logo192.png"
+            ></img>
+          </Link>
+        </Col>
+        <Col span={4}>
+          <Search placeholder="Search..." onSearch={onSearch} allowClear />
+        </Col>
+        <Col span={8} className="right-nav">
+          <Menu
+            className="navigator"
+            onClick={handleClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+          >
+            <Menu.Item key="searchbar"></Menu.Item>
+            <Menu.Item key="history">Kiểm tra Đơn hàng</Menu.Item>
+            <Menu.Item key="login" icon={<UserOutlined />}>
+              Đăng nhập
+            </Menu.Item>
+            <Menu.Item key="cart" icon={<ShoppingCartOutlined />}>
+              Giỏ hàng
+            </Menu.Item>
+          </Menu>
+        </Col>
+      </Router>
     </Row>
   );
 };
