f14a568ecf812430f6f4df327db8146b214c8b34
Jul <69659361+julsu2905@users.noreply.github.com>
2021-03-26 08:38:55 +0700

    update Login form layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import "antd/dist/antd.css";
 import "./App.css";
 import { Layout, Col, Row } from "antd";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigator from "./components/Navigator";
 import Home from "./components/Home";
 import Landing from "./components/Landing";
+import LoginForm from './components/LoginForm';
 const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
   return (
     <Router>
       <Layout>
@@ -20,6 +23,9 @@ function App() {
           </Route>
           <Route exact path="/">
             <Landing />
+          </Route>
+          <Route exact path="/">
+            <LoginForm />
           </Route>
         </Content>
         <Footer>Footer</Footer>
---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,74 @@
+import { Form, Col, Row } from "antd";
+import React, { useState } from "react";
+
+const LoginForm = () => {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    <Row className="container">
+      <Col className="login-form-wrapper" span={8}>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Username!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
+  );
+};
+
+export default LoginForm;
