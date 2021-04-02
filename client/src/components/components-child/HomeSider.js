07a389dc2548991aae39f15c40c38b96c56e6f4b
Phuong26071999 <69885490+Phuong26071999@users.noreply.github.com>
2021-04-02 15:45:08 +0700

    update Homepage: layout

--- client/src/components/components-child/HomeSider.js
+++ client/src/components/components-child/HomeSider.js
@@ -21,7 +21,7 @@ const HomeSider = () => {
        console.log("1");
       };
     return(
-        <div style={{ width: 300 }}>
+        <div style={{ width: 'aotu' }}>
         {/* <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
           {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
           
@@ -35,12 +35,12 @@ const HomeSider = () => {
         >
           <Row>
             <Col className="avatar">
-              <Avatar size={100} icon={<UserOutlined />} />
+              <Avatar size={110} icon={<UserOutlined />} />
               
             </Col>
           </Row>
           <Row>
-            <Col offset={7} className="user">
+            <Col offset={6} span={11} className="user">
               <h1>
                   Username
               </h1>
